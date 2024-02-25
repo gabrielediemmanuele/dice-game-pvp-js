c7659c138951463236b52637af4dae172990ec35
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-02-25 11:31:04 +0100

    feat: starting setup, add .hidden to css, starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Elements selected
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+//Starting Conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
