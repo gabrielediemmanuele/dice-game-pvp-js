614cfd242e19e9db21dba813e291ab3955686d23
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-02-25 12:05:29 +0100

    feat: add score system

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 //* Elements selected
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
+
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+
+const player1 = document.getElementById('name--1');
+const player2 = document.getElementById('name--2');
+
 const diceElement = document.querySelector('.dice');
 const newGameButton = document.querySelector('.btn--new');
 const diceRollButton = document.querySelector('.btn--roll');
@@ -12,6 +19,8 @@ console.log(diceElement);
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+let currentScore = 0;
 
 //* Rolling dice functionality
 diceRollButton.addEventListener('click', function () {
@@ -23,4 +32,11 @@ diceRollButton.addEventListener('click', function () {
   diceElement.src = diceImage; //c Add to the image src the string.
 
   // 3) Check for rolled 1: if true, switch to next player
+  if (diceNumber !== 1) {
+    // Add dice to current score
+    currentScore += diceNumber;
+    currentScore0.textContent = currentScore;
+  } else {
+    // Add dice to current score
+  }
 });
